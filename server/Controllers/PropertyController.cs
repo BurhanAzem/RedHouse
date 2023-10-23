e8fcbbb932045966a9186e35cbd3d3255cfa172d
burhanazem <burhan12sab@gmail.com>
2023-10-23 21:03:25 +0300

    save commit

--- server/Controllers/PropertyController.cs
+++ server/Controllers/PropertyController.cs
@@ -4,6 +4,7 @@ using RedHouse_Server.Services;
 using Cooking_School.Core.ModelUsed;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using server.Dtos.PropertyDtos;
 
 namespace RedHouse_Server.Controllers
 {
@@ -13,7 +14,7 @@ namespace RedHouse_Server.Controllers
     {
         private IPropertyServices _propertyServices;
 
-        public PropertyController(IPropertyServices propertyServices) 
+        public PropertyController(IPropertyServices propertyServices)
         {
             _propertyServices = propertyServices;
         }
@@ -30,7 +31,80 @@ namespace RedHouse_Server.Controllers
             if (result.Exception != null)
             {
                 var code = result.StatusCode;
-                 throw new StatusCodeException(code.Value, result.Exception);
+                throw new StatusCodeException(code.Value, result.Exception);
+            }
+            // else if(result.StatusCode == System.Net.HttpStatusCode.OK)
+            return Ok(result);
+
+        }
+
+
+        [HttpGet("/properties/{id}")]
+        public async Task<IActionResult> GetProperty(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _propertyServices.GetProperty(id);
+            if (result.Exception != null)
+            {
+                var code = result.StatusCode;
+                throw new StatusCodeException(code.Value, result.Exception);
+            }
+            // else if(result.StatusCode == System.Net.HttpStatusCode.OK)
+            return Ok(result);
+
+        }
+
+        [HttpGet("/properties")]
+        public async Task<IActionResult> GetAllProperties([FromQuery] FilterDto filterDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _propertyServices.GetProperties(filterDto);
+            if (result.Exception != null)
+            {
+                var code = result.StatusCode;
+                throw new StatusCodeException(code.Value, result.Exception);
+            }
+            // else if(result.StatusCode == System.Net.HttpStatusCode.OK)
+            return Ok(result);
+
+        }
+        [HttpDelete("/properties/{id}")]
+        public async Task<IActionResult> DeleteProperty(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _propertyServices.DeleteProperty(id);
+            if (result.Exception != null)
+            {
+                var code = result.StatusCode;
+                throw new StatusCodeException(code.Value, result.Exception);
+            }
+            // else if(result.StatusCode == System.Net.HttpStatusCode.OK)
+            return Ok(result);
+
+        }
+
+
+        [HttpPut("/properties/{id}")]
+        public async Task<IActionResult> UpdateProperty([FromBody] PropertyDto propertyDto, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _propertyServices.UpdateProperty(propertyDto, id);
+            if (result.Exception != null)
+            {
+                var code = result.StatusCode;
+                throw new StatusCodeException(code.Value, result.Exception);
             }
             // else if(result.StatusCode == System.Net.HttpStatusCode.OK)
             return Ok(result);
