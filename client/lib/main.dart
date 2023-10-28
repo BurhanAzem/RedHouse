7117e78929b27bdc011373a0e3caf9d216c44549
Ayman <ayman.dwikat.2001@gmail.com>
2023-10-28 16:33:04 +0300

    save commit

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: "Poppins",
       ),
-      home: BottomBar(),
+      home: BottomBar(), 
       routes: routes,
     );
   }
